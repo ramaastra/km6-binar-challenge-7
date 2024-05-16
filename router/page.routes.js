4dd94990e327094eb27343ac57f5a0aa8ff854a0
Rama Astra <ramaastra333@gmail.com>
2024-05-16 17:18:54 +0800

    feat: handle no token in reset password page

--- router/page.routes.js
+++ router/page.routes.js
@@ -14,6 +14,8 @@ router.get('/email', restrict, (req, res) => {
 });
 
 router.get('/reset-password', (req, res) => {
+  const { token } = req.query;
+  if (!token) req.flash('info', 'invalid');
   res.render('forgot-password/reset');
 });
 
