5122f32066f209673703592918cb9c84015bfecc
Rama Astra <ramaastra333@gmail.com>
2024-05-16 15:36:42 +0800

    feat: add email reset link and reset password page

--- router/page.routes.js
+++ router/page.routes.js
@@ -1,8 +1,20 @@
 const { Router } = require('express');
+const jwt = require('jsonwebtoken');
+const restrict = require('../middlewares/restrict');
 const router = Router();
 
 router.get('/forgot-password', (req, res) => {
   res.render('forgot-password');
+});
+
+router.get('/email', restrict, (req, res) => {
+  const { id, name } = req.user;
+  const token = jwt.sign({ id }, process.env.JWT_SECRET);
+  res.render('forgot-password/email', { token, name });
+});
+
+router.get('/reset-password', (req, res) => {
+  res.render('forgot-password/reset');
 });
 
 module.exports = router;
