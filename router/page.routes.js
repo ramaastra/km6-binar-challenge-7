fef71ae1593594e120980b6da3f150a4f7c546fb
Rama Astra <ramaastra333@gmail.com>
2024-05-16 19:32:07 +0800

    refactor: remove unused GET /email endpoint

--- router/page.routes.js
+++ router/page.routes.js
@@ -1,16 +1,8 @@
 const { Router } = require('express');
-const jwt = require('jsonwebtoken');
-const restrict = require('../middlewares/restrict');
 const router = Router();
 
 router.get('/forgot-password', (req, res) => {
   res.render('forgot-password');
-});
-
-router.get('/email', restrict, (req, res) => {
-  const { id, name } = req.user;
-  const token = jwt.sign({ id }, process.env.JWT_SECRET);
-  res.render('forgot-password/email', { token, name });
 });
 
 router.get('/reset-password', (req, res) => {
